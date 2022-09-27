4db508eab3c770c45942c932ad9b8d2868fc9679
Mr:cooder <mrcooder100@gmail.com>
2022-09-27 10:27:46 +0600

    Nothing

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1 align='center' className='head'>Mr : Cooder<span className='span'> First React App</span></h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
